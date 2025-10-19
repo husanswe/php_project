1fb26e7a1f98480a72908623460e85db121d1497
husanswe <sulaymonovhusan2002@gmail.com>
2025-10-19 12:03:58 +0500

    refactor create_post.php form structure and labels for clarity

--- create_post.php
+++ create_post.php
@@ -6,21 +6,24 @@
 <div class="container-xxl my-5 px-5 vh-100">
     
     <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+
         <div class="container-fluid py-5">
             <h1 class="display-5 fw-bold">Create post</h1>
         </div>
-        <div>
+
+        <form>
             <div class="mb-3 my-5">
-                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                <label for="exampleFormControlInput1" class="form-label">Title</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Post Title">
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <label class="form-label">Content</label>
+                <textarea class="form-control" rows="3"></textarea>
             </div>
-        </div>
-    </div>
+        </form>
 
+        <button type="button" class="btn btn-primary">Save</button>
+    </div>
 </div>
 
 <?php require 'includes/footer.php'; ?>
