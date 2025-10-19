4080f8e8848f47c92b1b64d92f600c7214b63118
husanswe <sulaymonovhusan2002@gmail.com>
2025-10-19 12:31:56 +0500

    refactor create_post.php form to include method and action attributes, and fix button type for submission

--- create_post.php
+++ create_post.php
@@ -1,9 +1,13 @@
 <?php
     $title = "Create Post"; 
     require 'includes/header.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        var_dump($_POST);
+    }
 ?>
 
-<div class="container-xxl my-5 px-5 vh-100">
+<div class="container-xxl container-fluid my-5 px-5 vh-100">
     
     <div class="p-5 mb-4 bg-body-tertiary rounded-3">
 
@@ -11,18 +15,18 @@
             <h1 class="display-5 fw-bold">Create post</h1>
         </div>
 
-        <form>
+        <form method="post" action="">
             <div class="mb-3 my-5">
-                <label for="exampleFormControlInput1" class="form-label">Title</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Post Title">
+                <label class="form-label">Title</label>
+                <input name="title" type="text" class="form-control" placeholder="Post Title">
             </div>
             <div class="mb-3">
                 <label class="form-label">Content</label>
-                <textarea class="form-control" rows="3"></textarea>
+                <textarea name="content" class="form-control" rows="3"></textarea>
             </div>
         </form>
 
-        <button type="button" class="btn btn-primary">Save</button>
+        <button type="submit" class="btn btn-primary">Save</button>
     </div>
 </div>
 
