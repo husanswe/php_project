5785dbc5ec0322c2edfdd3a11bc76bd3ce09e3b2
husanswe <sulaymonovhusan2002@gmail.com>
2025-11-29 16:00:06 +0500

    edited database info

--- database.php
+++ database.php
@@ -5,11 +5,14 @@
     $database = getenv('MYSQLDATABASE') ?: 'railway';
     $port = getenv('MYSQLPORT') ?: '3306';
     
-
+    $dsn = "mysql:host=$servername;port=$port;dbname=$database;charset=utf8mb4";
+    
     try {
-        $pdo = new PDO("mysql:host=$servername; dbname = $database", $username, $password);
-        // set the PDO error mode to exception
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = new PDO($dsn, $username, $password, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ]);
         echo "Connected successfully";
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
